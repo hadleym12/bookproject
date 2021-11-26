eb5bc0a4ace9a740181ca7c1032c91d740094f7e
Hadley Calder <hadleymcalder@gmail.com>
2021-11-26 17:19:48 -0500

    Product Webapp Tutorial

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, {Component} from 'react'
-import Table from './Table'
+import Clock from './Clock'
+import Products from './Products'
 
 class App extends Component {
-  render() {
-    return (
-        <div className="container">
-            <Table />
-        </div>
-    )
-  }
+    render() {
+        return (
+            <div className="App">
+                
+                <Clock/>
+
+                <section>
+                    <Products/>
+                </section>
+                
+
+            </div>
+        );
+    }
 }
 
 export default App
